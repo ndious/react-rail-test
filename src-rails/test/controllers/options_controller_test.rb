106d3461ee51b44529c8e60dd20d05488619929c
Nicolas Dufreche <ndufreche@clever-age.com>
2022-05-25 10:35:19 +0200

    add test to options_controller

--- src-rails/test/controllers/options_controller_test.rb
+++ src-rails/test/controllers/options_controller_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class OptionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "Admin should be able list all options available" do
+    authenticate_admin
+    get "/options", headers: { Authorization: @token_admin }
+
+    assert_response 200
+    assert_equal Option.all.count, json_response.count
+  end
+
+  test "Admin should be able to see a option" do
+    authenticate_admin
+    option = options(:option_one)
+    get "/options/#{option.id}", headers: { Authorization: @token_admin }
+
+    assert_response 200
+    assert_equal option.id, json_response["id"]
+  end
 end
